b06853f0bfafff51b0f5ec6ceac5c4a00eeaecc8
Harrie Lutz <harrielutz@gmail.com>
2017-06-09 16:13:16 +0200

    Added questionnaire edit option

--- hmac_scenario/server.py
+++ hmac_scenario/server.py
@@ -16,22 +16,24 @@ c = sqldb.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS users (username, password)');
 c.execute('CREATE UNIQUE INDEX IF NOT EXISTS users_ind0 ON users (username)');
 c.execute('CREATE TABLE IF NOT EXISTS questionnaire (secret_key, secret_data)');
-c.execute('CREATE INDEX IF NOT EXISTS questionnaire_ind0 ON questionnaire (secret_key)');
+c.execute('CREATE UNIQUE INDEX IF NOT EXISTS questionnaire_ind0 ON questionnaire (secret_key)');
 sqldb.commit()
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET'])
+def index_get():
     return render_template('index.html')
 
 
-@app.route('/register', methods=['GET','POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register_form.html')
+@app.route('/register', methods=['GET'])
+def register_get():
+    return render_template('register_form.html')
 
+
+@app.route('/register', methods=['POST'])
+def register_post():
     username = request.form.get('username', None)
     if username is None or not username.isalnum():  # .isalnum() also rejects empty strings
         return render_template('register_form.html', error='Invalid "username" value')
@@ -43,8 +45,7 @@ def register():
     # - Should check that username doesn't already exist
     # - Should allow punctuation etc. in password
 
-    password_enc = password.encode('utf-8')
-    secret_hmac = hmac.new(password_enc,username.encode('utf-8') , "sha256")
+    secret_hmac = hmac.new(password.encode('utf-8'), username.encode('utf-8'), 'sha256')
 
     c = sqldb.cursor()
     c.execute('INSERT INTO users (username, password) VALUES (?, ?)',
@@ -55,15 +56,17 @@ def register():
 
     resp = make_response(render_template('register_ok.html'))
     resp.set_cookie('username', username)
-    resp.set_cookie('secret_hmac', 'hmac:\t{}'.format(secret_hmac.hexdigest()))
+    resp.set_cookie('secret_hmac', secret_hmac.hexdigest())
     return resp
 
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login_form.html')
+@app.route('/login', methods=['GET'])
+def login_get():
+    return render_template('login_form.html')
 
+
+@app.route('/login', methods=['POST'])
+def login_post():
     username = request.form.get('username', None)
     if username is None or not username.isalnum():  # .isalnum() also rejects empty strings
         return render_template('login_form.html', error='Invalid "username" value')
@@ -73,34 +76,58 @@ def login():
 
     c = sqldb.cursor()
     c.execute('SELECT password FROM users WHERE username=?', (username,));
-    userpw = c.fetchone()
+    row = c.fetchone()
 
-    if userpw is not None:
-        if password == userpw[0]:
-            secret_hmac = hmac.new(password.encode('utf-8'),username.encode('utf-8') , "sha256")
-        else:
-            return render_template('login_form.html', error='Unknown username or bad password')
-    else:
+    if row is None or password != row[0]:
         return render_template('login_form.html', error='Unknown username or bad password')
+
+    secret_hmac = hmac.new(password.encode('utf-8'), username.encode('utf-8'), 'sha256')
 
     resp = make_response(render_template('login_ok.html', username=username, password=password))
     resp.set_cookie('username', username)
-    resp.set_cookie('secret_hmac', 'hmac:\t{}'.format(secret_hmac.hexdigest()))
+    resp.set_cookie('secret_hmac', secret_hmac.hexdigest())
     return resp
 
 
 @app.route('/show', methods=['GET'])
-def show():
+def show_get():
     username = request.cookies.get('username', None)
-    hmac = request.cookies.get('secret_hmac', None)
-    return render_template('show.html', username=username, hmac=hmac)
+    secret_hmac = request.cookies.get('secret_hmac', None)
+    return render_template('show.html', username=username, secret_hmac=secret_hmac)
+
+
+@app.route('/edit', methods=['GET'])
+def edit_get():
+    secret_hmac = request.cookies.get('secret_hmac', None)
+
+    c = sqldb.cursor()
+    c.execute('SELECT secret_data FROM questionnaire WHERE secret_key=?', (secret_hmac,));
+    row = c.fetchone()
+
+    if row is None:
+        return render_template('edit.html', secret_hmac=secret_hmac, error='Questionnaire data not found')
+
+    return render_template('edit.html', secret_hmac=secret_hmac, secret_data=row[0])
+
+
+@app.route('/edit', methods=['POST'])
+def edit_post():
+    secret_hmac = request.cookies.get('secret_hmac', None)
+
+    secret_data=request.form['secret_data']
+    c = sqldb.cursor()
+    c.execute('UPDATE questionnaire SET secret_data=? WHERE secret_key=?',
+              (secret_data, secret_hmac));
+    sqldb.commit();
+
+    return render_template('edit.html', secret_hmac=secret_hmac, secret_data=secret_data)
 
 
 @app.route('/logout', methods=['GET'])
 def logout():
     resp = make_response(render_template('logout_ok.html'))
     resp.set_cookie('username', '', 0)
-    resp.set_cookie('hmac', '', 0)
+    resp.set_cookie('secret_hmac', '', 0)
     return resp
 
 
--- hmac_scenario/test.py
+++ hmac_scenario/test.py
@@ -2,15 +2,18 @@
 
 import hashlib
 import hmac
+from pprint import pprint
+
+pprint(hmac)
 
 password = 'hallo'
-
 print('pw:\t{}'.format(password))
 
 password_enc = password.encode('utf-8')
 
-sk_hash = hashlib.sha256(secret_key)
+sk_hash = hashlib.sha256(password_enc)
 print('digest:\t{}'.format(sk_hash.hexdigest()))
 
 hmac = hmac.new("harrie".encode('utf-8'), password_enc, "sha256")
+pprint(hmac)
 print('hmac:\t{}'.format(hmac.hexdigest()))
